af2c6234db91d6eb43577bb032df85c8c44a62fb
Aula <Aula@EQUIP079587.xunta.local>
2020-12-11 10:57:03 +0100

    cambios funcionales

--- index.js
+++ index.js
@@ -6,8 +6,26 @@
  */
 function anoSanto(n) {
   //TODO: realizar cálculos oportunos
+  // 6 5 6 11
+
+  if (n>2100)n--;
+  while (n<2021) {
+    n+=28;
+  }
+  while (n>2038) n-=28;
+  
+
+
   if (n==2021) return true;
+  if (n==2027) return true;
+  if (n==2032) return true;
+  if (n==2038) return true;
+  if (n==2049) return true;
+
+
+
   return false;
+
 }
 
 
--- test.js
+++ test.js
@@ -2,6 +2,27 @@ import { anoSanto } from './index';
 import { strictEqual } from "assert";
 
 describe(('Comprobación de año santo'), () => {
-  it('test 2020', () => { strictEqual(anoSanto(2020), false) })
-  it('test 2021', () => { strictEqual(anoSanto(2021), true) })
+  it('test 2094 es santo', () => { strictEqual(anoSanto(2094), true) })
+
+  it('test 2020 no es santo', () => { strictEqual(anoSanto(2020), false) })
+  it('test 2021 es santo', () => { strictEqual(anoSanto(2021), true) })
+  it('test 2027 es santo', () => { strictEqual(anoSanto(2027), true) })
+  it('test 2032 es santo ', () => { strictEqual(anoSanto(2032), true) })
+  it('test 2033 no es santo', () => { strictEqual(anoSanto(2033), false) })
+  it('test 2038 es santo', () => { strictEqual(anoSanto(2038), true) })
+  it('test 2049 es santo', () => { strictEqual(anoSanto(2049), true) })
+  it('test 2094 es santo', () => { strictEqual(anoSanto(2094), true) })
+  it('test 2105 no es santo', () => { strictEqual(anoSanto(2105), true) })
+  it('test 2106 es santo', () => { strictEqual(anoSanto(2106), true) })
+
+
+  
+
+
+
+
+
+
+
+
 })
